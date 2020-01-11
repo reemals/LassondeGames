727cd87d643e18f6d3553e60270eed0bbb5528e3
reemals <reemals60@gmail.com>
2020-01-11 14:21:46 -0500

    Added some stuff

--- FrontEndDemoPython/mainClass.py
+++ FrontEndDemoPython/mainClass.py
@@ -38,18 +38,131 @@ def pathCost(cost,current_x, current_y, x,y):
 
     return cost
 
+def moveToMars():
+    ini = request.getData()
+    while (ini["location"]["x"] != ini["mapData"]["Mars"]["x"] or ini["location"]["y"] != ini["mapData"]["Mars"]["y"]):
+        ini = request.getData()
+        if (ini["location"]["y"] != ini["mapData"]["Mars"]["y"]):
+                if map[ini["location"]["x"], ini["location"]["y"] + 1] != -100:
+                    print(1)
+                    request.turn("N")
+                    request.move(1)
+                else:
+                    print(2)
+
+                    mats.asteroid(map)
+        elif ini["mapData"]["Mars"]["x"] != ini["location"]["x"]:
+                if map[ini["location"]["x"]+1, ini["location"]["y"] ] != -100:
+                    print(3)
+
+                    request.turn("E")
+                    request.move(1)
+                else:
+                    print(4)
+
+                    request.turn("E")
+                    mats.asteroid(map)
+    request.finish()
+
+def moveToStation1():
+    ini = request.getData()
+    while (ini["location"]["x"] != ini["mapData"]["Station1"]["x"] or ini["location"]["y"] != ini["mapData"]["Station1"]["y"]):
+        ini = request.getData()
+        if (ini["location"]["y"] != ini["mapData"]["Station1"]["y"]):
+            if map[ini["location"]["x"], ini["location"]["y"] + 1] != -100:
+                print(1)
+                request.turn("N")
+                request.move(1)
+            else:
+                print(2)
+
+                mats.asteroid(map)
+        elif ini["mapData"]["Mars"]["x"] != ini["location"]["x"]:
+            if map[ini["location"]["x"] + 1, ini["location"]["y"]] != -100:
+                print(3)
+
+                request.turn("E")
+                request.move(1)
+            else:
+                print(4)
+                request.turn("E")
+                mats.asteroid(map)
+
+    request.refuel()
+
+    ini = request.getData()
+    while (ini["location"]["x"] != ini["mapData"]["Mars"]["x"] or ini["location"]["y"] != ini["mapData"]["Mars"]["y"]):
+        ini = request.getData()
+        if (ini["location"]["y"] != ini["mapData"]["Mars"]["y"]):
+            if map[ini["location"]["x"], ini["location"]["y"] + 1] != -100:
+                print(1)
+                request.turn("N")
+                request.move(1)
+            else:
+                print(2)
+
+                mats.asteroid(map)
+        elif ini["mapData"]["Mars"]["x"] != ini["location"]["x"]:
+            if map[ini["location"]["x"] + 1, ini["location"]["y"]] != -100:
+                print(3)
+
+                request.turn("E")
+                request.move(1)
+            else:
+                print(4)
+
+                request.turn("E")
+                mats.asteroid(map)
+    request.finish()
+
+
+# def moveToStation2():
+#     while (ini["location"]["x"] != ini["mapData"]["Station2"]["x"] or ini["location"]["y"] !=
+#            ini["mapData"]["Station2"]["y"]):
+#         if (ini["location"]["y"] != ini["mapData"]["Station2"]["y"]):
+#             if map[ini["location"]["x"], ini["location"]["y"] + 1] != -100:
+#                 request.turn("N")
+#                 request.move(1)
+#             else:
+#                 mats.isAsteroid(map)
+#
+#         elif ini["mapData"]["Mars"]["x"] != ini["mapData"]["Station22"]["x"]:
+#             if map[ini["location"]["x"] + 1, ini["location"]["y"]] != -100:
+#                 request.turn("E")
+#                 request.move(1)
+#             else:
+#                 request.turn("E")
+#                 mats.isAsteroid(map)
+
+pathChosen = 0
 
 directPathToMars = pathCost(0, 0, 0, ini["mapData"]["Mars"]["x"], ini["mapData"]["Mars"]["y"])
 print("Cost1: " + str(directPathToMars))
+pathChosen = 1
 
-# if directPathToMars > 30 :
-pathToStation2 =  pathCost(0, 0, 0, ini["mapData"]["Station2"]["x"], ini["mapData"]["Station2"]["y"])
-pathfromoStation2ToMars = pathCost(0, ini["mapData"]["Station2"]["x"], ini["mapData"]["Station2"]["y"], ini["mapData"]["Mars"]["x"], ini["mapData"]["Mars"]["y"])
-TotalStation2 = pathfromoStation2ToMars + pathToStation2
-print("Cost2: " + str(TotalStation2))
+if directPathToMars > 30 :
+    pathToStation2 =  pathCost(0, 0, 0, ini["mapData"]["Station2"]["x"], ini["mapData"]["Station2"]["y"])
+    pathfromoStation2ToMars = pathCost(0, ini["mapData"]["Station2"]["x"], ini["mapData"]["Station2"]["y"], ini["mapData"]["Mars"]["x"], ini["mapData"]["Mars"]["y"])
+    TotalStation2 = pathfromoStation2ToMars + pathToStation2
+    print("Cost2 pathToStation2: " + str(pathToStation2))
+    print("Cost2 pathfromoStation2ToMars: " + str(pathfromoStation2ToMars))
+    print("Cost2 TotalThroughStation2: " + str(TotalStation2))
+    pathChosen = 2
 
-# if TotalStation2 > 30 :
-pathToStation1 =  pathCost(0, 0, 0, ini["mapData"]["Station1"]["x"], ini["mapData"]["Station1"]["y"])
-pathfromoStation1ToMars = pathCost(0, ini["mapData"]["Station1"]["x"], ini["mapData"]["Station1"]["y"], ini["mapData"]["Mars"]["x"], ini["mapData"]["Mars"]["y"])
-TotalStation1 = pathfromoStation1ToMars + pathToStation1
-print("Cost3: " + str(TotalStation1))
+if pathToStation2 > 30 or pathfromoStation2ToMars > 30:
+    pathToStation1 =  pathCost(0, 0, 0, ini["mapData"]["Station1"]["x"], ini["mapData"]["Station1"]["y"])
+    pathfromoStation1ToMars = pathCost(0, ini["mapData"]["Station1"]["x"], ini["mapData"]["Station1"]["y"], ini["mapData"]["Mars"]["x"], ini["mapData"]["Mars"]["y"])
+    TotalStation1 = pathfromoStation1ToMars + pathToStation1
+    print("Cost3 pathToStation1: " + str(pathToStation1))
+    print("Cost3 pathfromoStation1ToMars: " + str(pathfromoStation1ToMars))
+    print("Cost3 TotalThroughStation1: " + str(TotalStation1))
+    pathChosen = 3
+
+print("pathChosen: " + str(pathChosen))
+
+# if pathChosen == 1:
+#     moveToMars()
+# elif pathChosen == 2:
+moveToStation1()
+# else:
+#     moveToStation2()
